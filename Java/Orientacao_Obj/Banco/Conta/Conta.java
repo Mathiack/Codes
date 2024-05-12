0450b41e39659dd642b5eb02fa5627e93ef6254f
Thunder <151688223+Guilherme-Thunder@users.noreply.github.com>
2024-05-12 11:14:45 -0300

    me estressei um pouco mais com esse VS Codigos que arruma problema no meio do NADA aaaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ Java/Orientacao_Obj/Banco/Conta/Conta.java
@@ -0,0 +1,8 @@
+package Orientacao_Obj./*saldo, agencia, numero, titular*/
+public class conta {
+    double saldo;
+    int agencia;
+    int numero;
+    String titular;
+
+}
---
+++ Java/Orientacao_Obj/Banco/Conta/CriarConta.java
@@ -0,0 +1,8 @@
+package Orientacao_Obj.Banco.Conta;
+
+public class CriarConta {
+    public static void main (String [] args) {
+        Conta primeira_Conta = new Conta();
+        primeiraconta.saldo = 200;
+    }
+}
