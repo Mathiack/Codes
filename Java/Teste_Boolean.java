450199e5bdce4b7e825f87e7618301cad62104f7
Thunder <151688223+Guilherme-Thunder@users.noreply.github.com>
2024-05-12 10:46:34 -0300

    boolean, while e for

---
+++ Java/Teste_Boolean.java
@@ -0,0 +1,19 @@
+public class Teste_Boolean {
+    public static void main(String[] args) {
+        System.out.println("Testando condicionais");
+        int idade = 10;
+        int quant_Pessoa = 1;
+        boolean acompanhado;
+
+        if (quant_Pessoa >= 2 ) {
+            acompanhado = true;
+        } else {
+            acompanhado = false;
+        }
+        if (idade >= 18 && acompanhado == true) {
+            System.out.println("Você pode comprar");
+        } else {
+            System.out.println("Você não pode comprar");
+        }
+    }
+}
---
+++ Java/Teste_For.java
@@ -0,0 +1,17 @@
+public class Teste_For {
+    public static void main (String [] args) {
+
+        for (int contador = 0; contador <= 10; contador ++) {
+            /*
+            é o mesmo que:
+            while (contador <= 10) {
+            System.out.println(contador);
+            contador += 1;
+            //contador = contador + 1;
+            //contador ++;
+            } */
+            System.out.println(contador);
+        }
+        // se fizer o sout aqui fora da errado, pq foi inicializado dentro do FOR;
+    }
+}
