924e5e6b8552f6d50718bf73f80c943767656f4d
Thunder <151688223+Guilherme-Thunder@users.noreply.github.com>
2024-06-13 20:23:57 -0300

    operadores relacionais, etc

---
+++ JavaScript/CFBCursos/Operadores/Relacionais/script.js
@@ -0,0 +1,18 @@
+/*  >
+    <
+    >=
+    <=
+    == ---> comparação
+    =  ---> atribuição
+    !=
+    */
+
+let n1 = 10
+let n2 = 5
+let n3 = 10
+
+console.log(n1 != n3)
+console.log(n1 != n2)
+console.log(n2 <= n3)
+console.log(n1 > n3)
+console.log(n1 = n2)
