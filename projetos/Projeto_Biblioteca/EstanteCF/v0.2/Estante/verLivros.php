3debf817c3f621e2be60b2404521cf0f358cf197
Thunder <151688223+Guilherme-Thunder@users.noreply.github.com>
2024-07-07 15:41:04 -0300

    arrumando a tabela do Ver Livros

--- projetos/Projeto_Biblioteca/EstanteCF/v0.2/Estante/verLivros.php
+++ projetos/Projeto_Biblioteca/EstanteCF/v0.2/Estante/verLivros.php
@@ -1,10 +1,3 @@
-<?php
-    session_start();
-
-    include "biblioteca.class.php";
-    $b = new Biblioteca();
-    $livros = $b->getLivrosByIddoUser($ID_User);
-?>
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -14,44 +7,44 @@
     <title>Ver Livros</title>
     <link rel="stylesheet" href="../style.css">
     <style>
-    * {
-        font-family: monospace;
-    }
+        * {
+            font-family: monospace;
+        }
 
-    a#voltarVerLivros {
-        text-decoration: none;
-        padding: 5px;
-        background-color: gray;
-        border-radius: 10px;
-        color: white;
-    }
+        a#voltarVerLivros {
+            text-decoration: none;
+            padding: 5px;
+            background-color: gray;
+            border-radius: 10px;
+            color: white;
+        }
 
-    div#voltaVerLivros {
-        margin-top: 20px;
-    }
+        div#voltaVerLivros {
+            margin-top: 20px;
+        }
 
-    a.opsTable#atualizar {
-        color: rgb(0, 102, 204);
-        text-decoration: none;
-    }
+        a.opsTable#atualizar {
+            color: rgb(0, 102, 204);
+            text-decoration: none;
+        }
 
-    a.opsTable#apagar {
-        color: red;
-        text-decoration: none;
-    }
+        a.opsTable#apagar {
+            color: red;
+            text-decoration: none;
+        }
 
-    table,
-    th,
-    td {
-        border: 1px solid black;
-        border-collapse: collapse;
-        padding: 4px;
-    }
+        table,
+        th,
+        td {
+            border: 1px solid black;
+            border-collapse: collapse;
+            padding: 4px;
+        }
 
-    a#cliqueAQ {
-        text-decoration: underline;
-        color: black;
-    }
+        a#cliqueAQ {
+            text-decoration: underline;
+            color: black;
+        }
     </style>
 </head>
 
@@ -62,19 +55,21 @@
                 <h1>Livros Adicionados</h1>
             </div>
             <?php
-                $numLivros = 0;
-                foreach ($livros as $book) {
-                        $numLivros++;
-                }
-                if($numLivros == 0) {
+            session_start();
+
+            include "biblioteca.class.php";
+            $b = new Biblioteca();
+            $livros = $b->getLivrosByIddoUser($ID_User);
+
+            $numLivros = count($livros);
+            if ($numLivros == 0) {
                 echo "
                     <p>Você ainda não tem nenhum Livro!<p>
                     <p><a href='../Ops/addLivros.php' id='cliqueAQ'>Clique aqui</a> para adicionar um livro</p>
-                    ";
-                }
-                else {
+                ";
+            } else {
+                echo "<p>Número de Livros: $numLivros</p>";
             ?>
-                <p>Número de Livros: <?php echo $numLivros?></p>
                 <table>
                     <thead>
                         <tr>
@@ -85,29 +80,31 @@
                             <th colspan="2">Operações</th>
                         </tr>
                     </thead>
-                    <?php
-                        foreach ($livros as $livro){
-                    ?>
                     <tbody>
-                        <tr>
-                            <td><?php echo $livro['Nome']; ?></td>
-                            <td><?php echo $livro['Autor']; ?></td>
-                            <td><?php echo $livro['ISBN']; ?></td>
-                            <td><?php echo $livro['Editora']?></td>
-                            <td><?php echo "<a href='../Ops/apagar.php?id=". $livro['ID_Livro']. "' class='opsTable' id='apagar'>Apagar🚫</a>"?></td>
-                            <td><?php echo "<a href='../Ops/att.php?id=". $livro['ID_Livro']. "' class='opsTable' id='atualizar'>Editar✏️</a>"?></td>
-                        </tr>
+                        <?php
+                        foreach ($livros as $livro) {
+                        ?>
+                            <tr>
+                                <td><?php echo $livro['Nome']; ?></td>
+                                <td><?php echo $livro['Autor']; ?></td>
+                                <td><?php echo $livro['ISBN']; ?></td>
+                                <td><?php echo $livro['Editora']; ?></td>
+                                <td><?php echo "<a href='../Ops/apagar.php?id=" . $livro['ID_Livro'] . "' class='opsTable' id='apagar'>Apagar🚫</a>"; ?></td>
+                                <td><?php echo "<a href='../Ops/att.php?id=" . $livro['ID_Livro'] . "' class='opsTable' id='atualizar'>Editar✏️</a>"; ?></td>
+                            </tr>
+                        <?php
+                        }
+                        ?>
                     </tbody>
                 </table>
             <?php
-                        }
-                }
+            }
             ?>
-            </table>
             <div id="voltaVerLivros">
                 <a href="index.php" id="voltarVerLivros">Voltar à Tela Inicial</a>
             </div>
         </div>
     </div>
 </body>
+
 </html>
